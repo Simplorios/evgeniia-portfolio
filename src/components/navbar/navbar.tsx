8c8299b990a4109a6346efbb113c78ac3af57fb8
Березовский Ярослав Владимирович <YBerezovskiy@alfabank.ru>
2025-03-22 15:42:15 +0300

    feat(*): added shadow to main header

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,22 +6,24 @@ import { Button } from '~/components/button'
 
 export const Navbar = () => {
   return (
-    <div className="h-[99] container mx-auto flex justify-between items-center">
-      <Image src="/logo.svg" width={44} height={61} alt="Logo" />
-      <nav className="flex flex-col justify-center">
-        <ul className="inline-grid grid-flow-col auto-rows-min gap-17">
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Projects</a>
-          </li>
-          <li>
-            <a href="#">Process</a>
-          </li>
-        </ul>
-      </nav>
-      <Button>Contact</Button>
-    </div>
+    <header className="shadow-header">
+      <div className="h-[99] container mx-auto flex justify-between items-center">
+        <Image src="/logo.svg" width={44} height={61} alt="Logo" />
+        <nav className="flex flex-col justify-center">
+          <ul className="inline-grid grid-flow-col auto-rows-min gap-17">
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Process</a>
+            </li>
+          </ul>
+        </nav>
+        <Button>Contact</Button>
+      </div>
+    </header>
   )
 }
